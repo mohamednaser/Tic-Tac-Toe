63cc5d3950d84c2d0dd3bc789d465a3dc0868f2b
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-27 03:46:58 +0200

    add winner message

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,28 @@
-document.getElementById("game-form").addEventListener(
+// Start Game
+document.getElementById("start-game").addEventListener(
   "click",
   function (event) {
     event.preventDefault();
-    document.getElementById("game-form").className = "d-none";
+    document.getElementById("game-players-form").className = "d-none";
+    document.getElementById("game-container").className = "game-container";
   },
   false
 );
 
+// Retart Game
+document.getElementById("finish-game").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    document.getElementById("game-players-form").className = "m-auto w-50";
+    document.getElementById("game-container").className = "d-none";
+  },
+  false
+);
 function startGame() {}
 
 function chooseBoardCell(selectedCell) {
   selectedCell.innerHTML = "X";
+  selectedCell.style.cursor = "not-allowed";
   console.log(selectedCell.id, "selectedCell details ");
 }
