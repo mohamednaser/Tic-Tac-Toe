8325cbd9903ad53333698b638ab1db43b6699552
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-27 02:55:47 +0200

    add files

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+document.getElementById("game-form").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    document.getElementById("game-form").className = "d-none";
+  },
+  false
+);
+
+function startGame() {}
