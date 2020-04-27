9007ac112eb8c72b67299cabcf2b7d72786f8ac5
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-27 20:32:39 +0200

    fix eslint issues

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,31 @@
 // Global Tic-tak-toe Variables
 let boardArray;
-let boardArrayDiv = document.getElementById("game-board");
-let currentPlayer = "X";
+const boardArrayDiv = document.getElementById('game-board');
+let currentPlayer = 'X';
 
 const player = {
   getName: (marker) => {
-    if (marker == "X") return document.getElementById("playerX").value;
-    else return document.getElementById("playerO").value;
+    if (marker === 'X') return document.getElementById('playerX').value;
+    return document.getElementById('playerO').value;
   },
 };
 
-let playerX = player;
-let playerO = player;
+const playerX = player;
+const playerO = player;
 
 const board = {
   initialize: () => {
     boardArray = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
     ];
   },
 
   render: () => {
-    for (let x = 0; x <= 2; x++) {
-      for (let y = 0; y <= 2; y++) {
-        let htmlCeil = `<div class='board-cell cursor-pointer' onclick="game.move(this)" id="${x},${y}"> ${boardArray[x][y]}</div>`;
+    for (let x = 0; x <= 2; x += 1) {
+      for (let y = 0; y <= 2; y += 1) {
+        const htmlCeil = `<div class='board-cell cursor-pointer' onclick="game.move(this)" id="${x},${y}"> ${boardArray[x][y]}</div>`;
 
         boardArrayDiv.innerHTML += htmlCeil;
       }
@@ -33,8 +33,7 @@ const board = {
   },
 
   reset: () => {
-    console.log("players data ", playerX, playerO);
-    boardArrayDiv.innerHTML = "";
+    boardArrayDiv.innerHTML = '';
     board.initialize();
     board.render();
   },
@@ -42,24 +41,24 @@ const board = {
 
 const game = {
   start: () => {
-    document.getElementById("game-players-form").className = "d-none";
-    document.getElementById("game-container").className = "game-container";
+    document.getElementById('game-players-form').className = 'd-none';
+    document.getElementById('game-container').className = 'game-container';
 
-    playerX.name = document.getElementById("playerX").value;
-    playerO.name = document.getElementById("playerO").value;
+    playerX.name = document.getElementById('playerX').value;
+    playerO.name = document.getElementById('playerO').value;
 
-    playerX.marker = "X";
-    playerO.marker = "Y";
+    playerX.marker = 'X';
+    playerO.marker = 'Y';
 
     playerX.winscount = 0;
     playerO.winscount = 0;
 
     // Set Players Name
-    document.getElementById("playerXName").innerHTML = document.getElementById(
-      "playerX"
+    document.getElementById('playerXName').innerHTML = document.getElementById(
+      'playerX',
     ).value;
-    document.getElementById("playerOName").innerHTML = document.getElementById(
-      "playerO"
+    document.getElementById('playerOName').innerHTML = document.getElementById(
+      'playerO',
     ).value;
 
     board.reset();
@@ -67,26 +66,23 @@ const game = {
 
   move: (selectedCell) => {
     selectedCell.innerHTML = currentPlayer;
-    selectedCell.style.cursor = "not-allowed";
-    selectedCell.style.pointerEvents = "none";
-    selectedRowColumeArray = selectedCell.id.split(",");
-
+    selectedCell.style.cursor = 'not-allowed';
+    selectedCell.style.pointerEvents = 'none';
+    const selectedRowColumeArray = selectedCell.id.split(',');
     boardArray[selectedRowColumeArray[0]][
       selectedRowColumeArray[1]
     ] = currentPlayer;
     if (game.checkWin()) {
       // show winner and finish this round
-      alert(player.getName(currentPlayer) + " win !");
+      alert(`${player.getName(currentPlayer)} win !`);
       game.setWinner(currentPlayer);
     } else {
       game.switchPlayer();
-      console.log(selectedCell.id, "selectedCell details ", boardArray);
     }
   },
-  
 
   setWinner: (winner) => {
-    if (winner === "X") {
+    if (winner === 'X') {
       playerX.winscount += 1;
     } else {
       playerO.winscount += 1;
@@ -96,30 +92,16 @@ const game = {
 
   checkWin: () => {
     if (
-      (boardArray[0][0] != "" &&
-        boardArray[0][0] === boardArray[0][1] &&
-        boardArray[0][1] === boardArray[0][2]) ||
-      (boardArray[1][0] != "" &&
-        boardArray[1][0] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[1][2]) ||
-      (boardArray[2][0] != "" &&
-        boardArray[2][0] === boardArray[2][1] &&
-        boardArray[2][1] === boardArray[2][2]) ||
-      (boardArray[0][0] != "" &&
-        boardArray[0][0] === boardArray[1][0] &&
-        boardArray[1][0] === boardArray[2][0]) ||
-      (boardArray[0][1] != "" &&
-        boardArray[0][1] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[2][1]) ||
-      (boardArray[0][2] != "" &&
-        boardArray[0][2] === boardArray[1][2] &&
-        boardArray[1][2] === boardArray[2][2]) ||
-      (boardArray[0][0] != "" &&
-        boardArray[0][0] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[2][2]) ||
-      (boardArray[0][2] != "" &&
-        boardArray[0][2] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[2][0])
+      (boardArray[0][0] !== '' && boardArray[0][0] === boardArray[0][1]
+      && boardArray[0][1] === boardArray[0][2])
+      || (boardArray[1][0] !== ''
+      && boardArray[1][0] === boardArray[1][1] && boardArray[1][1] === boardArray[1][2])
+      || (boardArray[2][0] !== '' && boardArray[2][0] === boardArray[2][1] && boardArray[2][1] === boardArray[2][2])
+      || (boardArray[0][0] !== '' && boardArray[0][0] === boardArray[1][0] && boardArray[1][0] === boardArray[2][0])
+      || (boardArray[0][1] !== '' && boardArray[0][1] === boardArray[1][1] && boardArray[1][1] === boardArray[2][1])
+      || (boardArray[0][2] !== '' && boardArray[0][2] === boardArray[1][2] && boardArray[1][2] === boardArray[2][2])
+      || (boardArray[0][0] !== '' && boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2])
+      || (boardArray[0][2] !== '' && boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0])
     ) {
       return true;
     }
@@ -127,8 +109,8 @@ const game = {
   },
 
   restart: () => {
-    document.getElementById("game-container").className = "d-none";
-    document.getElementById("game-players-form").className = "m-auto w-50";
+    document.getElementById('game-container').className = 'd-none';
+    document.getElementById('game-players-form').className = 'm-auto w-50';
     board.reset();
   },
 
@@ -137,6 +119,6 @@ const game = {
   },
 
   switchPlayer: () => {
-    currentPlayer = currentPlayer == "X" ? "O" : "X";
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
   },
 };
