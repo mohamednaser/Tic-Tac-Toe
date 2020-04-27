013cf9e2ca0afbceecbd8353d482971306fa45e4
Mohamed Naser <nc_m.naser@hotmail.com>
2020-04-27 13:54:48 +0200

    build check winner function

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,37 @@
 // Global Tic-tak-toe Variables
 let boardArray;
 let winner;
-let playerXWinsCount = 04;
-let playerOWinsCount = 04;
-let gameBoardDiv = document.getElementById("game-board");
+let boardArrayDiv = document.getElementById("game-board");
+let currentPlayer = "X";
 
-function chooseBoardCell(selectedCell) {
-  selectedCell.innerHTML = "X";
-  selectedCell.style.cursor = "not-allowed";
-  console.log(selectedCell.id, "selectedCell details ");
-}
-
-const player = {
-  setName: (name) => {
-    this.name = name;
-  },
-  setMarker: (marker) => {
-    this.marker = marker;
-  },
-};
+const player = {};
 
 let playerX = player;
 let playero = player;
 
 const board = {
-  render: () => {
-    for (let x = 0; x <= 2; x++) {
-      for (let y = 0; y <= 2; y++) {
-        let htmlCeil = `<div class='board-cell cursor-pointer' onclick="chooseBoardCell(this)" id="${x}|${y}"> ${boardArray[x][y]}</div>`;
-
-        gameBoardDiv.innerHTML += htmlCeil;
-      }
-    }
-  },
-  reset: () => {
+  initialize: () => {
     boardArray = [
       ["", "", ""],
       ["", "", ""],
       ["", "", ""],
     ];
+  },
+
+  render: () => {
+    for (let x = 0; x <= 2; x++) {
+      for (let y = 0; y <= 2; y++) {
+        let htmlCeil = `<div class='board-cell cursor-pointer' onclick="game.move(this)" id="${x},${y}"> ${boardArray[x][y]}</div>`;
+
+        boardArrayDiv.innerHTML += htmlCeil;
+      }
+    }
+  },
+
+  reset: () => {
+    boardArrayDiv.innerHTML = "";
+    board.initialize();
+    board.render();
   },
 };
 
@@ -58,13 +51,69 @@ const game = {
     document.getElementById("playerOName").innerHTML = playerO.name;
 
     board.reset();
-    board.render();
+  },
+
+  move: (selectedCell) => {
+    selectedCell.innerHTML = currentPlayer;
+    selectedCell.style.cursor = "not-allowed";
+    selectedCell.style.pointerEvents = "none";
+    selectedRowColumeArray = selectedCell.id.split(",");
+
+    boardArray[selectedRowColumeArray[0]][
+      selectedRowColumeArray[1]
+    ] = currentPlayer;
+    if (game.checkWin()) {
+      // show winner and finish this round
+      alert(currentPlayer + "win");
+    } else {
+      game.switchPlayer();
+      console.log(selectedCell.id, "selectedCell details ", boardArray);
+    }
+  },
+
+  checkWin: () => {
+    if (
+      (boardArray[0][0] != "" &&
+        boardArray[0][0] === boardArray[0][1] &&
+        boardArray[0][1] === boardArray[0][2]) ||
+      (boardArray[1][0] != "" &&
+        boardArray[1][0] === boardArray[1][1] &&
+        boardArray[1][1] === boardArray[1][2]) ||
+      (boardArray[2][0] != "" &&
+        boardArray[2][0] === boardArray[2][1] &&
+        boardArray[2][1] === boardArray[2][2]) ||
+      (boardArray[0][0] != "" &&
+        boardArray[0][0] === boardArray[1][0] &&
+        boardArray[1][0] === boardArray[2][0]) ||
+      (boardArray[0][1] != "" &&
+        boardArray[0][1] === boardArray[1][1] &&
+        boardArray[1][1] === boardArray[2][1]) ||
+      (boardArray[0][2] != "" &&
+        boardArray[0][2] === boardArray[1][2] &&
+        boardArray[1][2] === boardArray[2][2]) ||
+      (boardArray[0][0] != "" &&
+        boardArray[0][0] === boardArray[1][1] &&
+        boardArray[1][1] === boardArray[2][2]) ||
+      (boardArray[0][2] != "" &&
+        boardArray[0][2] === boardArray[1][1] &&
+        boardArray[1][1] === boardArray[2][0])
+    ) {
+      return true;
+    }
+    return false;
   },
 
   restart: () => {
     document.getElementById("game-container").className = "d-none";
     document.getElementById("game-players-form").className = "m-auto w-50";
     board.reset();
-    board.render();
+  },
+
+  playAgain: () => {
+    board.reset();
+  },
+
+  switchPlayer: () => {
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
   },
 };
